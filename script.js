814ab9c06216a4ef1b7b24f25c416e635b837516
landgreen <rosslandgreen@gmail.com>
2023-12-01 10:37:43 -0800

    nsonosino

---
+++ script.js
@@ -0,0 +1,119 @@
+/* test on JS 
+you need to know how to use:
+
+if
+for
+var, let, const
+function
+array
+object
+*/
+
+//javascript challenge assignments
+
+//  challenge #0
+//fill an array with the numbers 0-99999
+//console.log the array
+//Example Output: [0,1,2,3,4,5, ... 99999]
+
+// let numbers = []
+// for (let i = 0; i < 99999; i++) {
+//   numbers.push(i);
+// }
+// console.log(numbers.length)
+
+
+
+//  challenge #1
+//fill an array with the numbers 0-99 counting by 3
+//console.log the array
+//Example Output: [0,3,6,9, ... 99]
+
+// let numbers = []
+// for (let i = 0; i < 99; i += 3) {
+//   numbers.push(i);
+// }
+// console.log(numbers.length)
+
+//  challenge #2
+//fill an array with the numbers 0-99 counting by 3
+//skip the numbers 9, 90, 12
+//console.log the array
+//Example Output: [0,3,6,12, ... 99]
+
+// let numbers = []
+// for (let i = 0; i < 100; i += 3) {
+//   // if( i !== 9 && i!==90  && i !== 12){
+//   if( i !== 9 && i!==90  && i !== 12){
+
+//   } else {
+//         numbers.push(i);
+//   }
+// }
+// console.log(numbers)
+
+//  challenge #3
+//fill an array with the numbers 0-99
+//alternate negative and positive
+//hint: use a modulus opperator
+//console.log the array
+//Example Output: [0,-1,2,-3,4,-5,6, ... 99]
+
+
+
+
+
+
+//  challenge #4
+//output the correct time with the date() method
+//https://www.w3schools.com/js/js_date_methods.asp
+//console.log the time hours:minutes:seconds
+//Example Output: "14:50:23"
+
+
+
+//  challenge #5
+//output the correct time with the date() method
+//use 12-hour time and AM/PM, not 24-hour time.
+//console.log the time hours:minutes:seconds
+//Example Output: "2:50:23 PM"
+
+
+
+
+
+//  challenge #6
+//output the correct time with the date() method
+//use 12-hour time and AM/PM, not 24-hour time.
+//output for the east time zone
+//console.log the time hours:minutes:seconds
+//Example Output: "5:50:23 PM"
+
+// OPTIONAL challenge #6.5
+//make a clock in canvas with an hour hand, minute hand, and second hand.
+//This requires a bit of trig, ask for help if you havn't learned sin and cos.
+//Example Output: a clock... just google clock if you don't know...  sigh...
+
+//  challenge #7
+//set a variable to a random digit: 0-9  (no decimals)
+//use a switch statement to store the word for each number to that variable
+//console.log the variable
+//Example Output if the random number is 4: "four"
+
+//  challenge #8
+//set a variable to a random number from 0-9999
+//set that variable to the words for each digit
+//console.log the variable
+//Example Output if the random number is 1451: "one four five one"
+
+// challenge #9
+//make a function takes in 3 numbers
+//make the function return the largest of the 3 numbers
+//console.log the function
+//Example Output if the numbers are 3,7,-22: 7
+
+// challenge #10
+//make a function takes in an array of at least 10 numbers
+//make the function return the largest of the numbers
+//console.log the function
+//Example Output if the array =[4,6.1,2,-10,30,100,200000,23,0,0,0,213,7.7]: 200000
